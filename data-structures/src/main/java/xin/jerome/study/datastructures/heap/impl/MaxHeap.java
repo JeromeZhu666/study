5467e3fc94ee5507ea28fc6f9a064b72b861e13a
JeromeZhu <JeromeZhu666@gmail.com>
2019-07-19 22:11:05 +0800

    完善注释

--- data-structures/src/main/java/xin/jerome/study/datastructures/heap/impl/MaxHeap.java
+++ data-structures/src/main/java/xin/jerome/study/datastructures/heap/impl/MaxHeap.java
@@ -4,10 +4,10 @@ import xin.jerome.study.datastructures.array.MyGenericArray;
 import xin.jerome.study.datastructures.heap.MyHeap;
 
 /**
- * 最大堆的数组实现
- * <br>二叉堆(若不加限定时，堆这种数据结构均指二叉堆这种实现):
- * <br>1.堆总是一棵完全二叉树,即按照层序从左到右依次排列.
- * <br>2.堆中任意节点的值总是不大于其父节点的值.
+ * 最大堆的数组实现 <br>
+ * 二叉堆(若不加限定时，堆这种数据结构均指二叉堆这种实现): <br>
+ * 1.堆总是一棵完全二叉树,即按照层序从左到右依次排列. <br>
+ * 2.堆中任意节点的值总是不大于其父节点的值.
  *
  * @author Jerome Zhu
  * @since 2019.07.01 21:57
@@ -27,13 +27,15 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
     /**
      * Heapify 将一个数组堆化
      *
-     * @param arr 待转化的数组
+     * @param arr
+     *            待转化的数组
      */
     public MaxHeap(E[] arr) {
         data = new MyGenericArray<>(arr);
         // 从最后一个非叶子节点,进行下沉操作. 时间复杂度O(n)
-        for (int i = getParentIndex(arr.length - 1); i >= 0 ; i--)
+        for (int i = getParentIndex(arr.length - 1); i >= 0; i--) {
             siftDown(i);
+        }
     }
 
     @Override
@@ -47,7 +49,8 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
     /**
      * 将index位置的元素进行上浮
      *
-     * @param index 指定位置
+     * @param index
+     *            指定位置
      */
     private void siftUp(int index) {
         // 若当前节点大于父节点,将当前节点与父节点进行交换.
@@ -73,8 +76,9 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
     @Override
     public E findMax() {
         // 如果当前堆为空,抛异常.
-        if (data.getSize() == 0)
+        if (data.getSize() == 0) {
             throw new IllegalArgumentException("Can't extractMax when heap is empty.");
+        }
         return data.get(0);
     }
 
@@ -91,7 +95,8 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
     /**
      * 将指定位置的元素下沉
      *
-     * @param index 指定的位置
+     * @param index
+     *            指定的位置
      */
     private void siftDown(int index) {
 
@@ -99,12 +104,13 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
         while (getLeftChildIndext(index) < data.getSize()) {
             int maxChildIndex = getLeftChildIndext(index);
             if (getRightChildIndex(index) < data.getSize()
-                    && data.get(getRightChildIndex(index)).compareTo(data.get(maxChildIndex)) > 0) {
+                && data.get(getRightChildIndex(index)).compareTo(data.get(maxChildIndex)) > 0) {
                 maxChildIndex = getRightChildIndex(index);
             }
             // 如果当前节点比最大节点还要大,不用交换了已经平衡了.
-            if (data.get(index).compareTo(data.get(maxChildIndex)) > 0)
+            if (data.get(index).compareTo(data.get(maxChildIndex)) > 0) {
                 break;
+            }
 
             // 否则进行交换
             data.swap(index, maxChildIndex);
@@ -127,19 +133,22 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
     /**
      * 获取指定节点的父亲节点的索引
      *
-     * @param index 指定节点的索引
+     * @param index
+     *            指定节点的索引
      * @return 父亲节点的索引
      */
     private int getParentIndex(int index) {
-        if (index == 0)
+        if (index == 0) {
             throw new IllegalArgumentException("index-0 doesn't have parent.");
+        }
         return (index - 1) / 2;
     }
 
     /**
      * 获取指定节点的左孩子节点的索引
      *
-     * @param index 指定节点的索引
+     * @param index
+     *            指定节点的索引
      * @return 左孩子节点的索引
      */
     private int getLeftChildIndext(int index) {
@@ -149,7 +158,8 @@ public class MaxHeap<E extends Comparable<E>> implements MyHeap<E> {
     /**
      * 获取指定节点的右孩子节点的索引
      *
-     * @param index 指定节点的索引
+     * @param index
+     *            指定节点的索引
      * @return 右孩子节点的索引
      */
     private int getRightChildIndex(int index) {
