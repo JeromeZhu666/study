6b66eca3333a9179a9b87be97d32e05fa303be14
JeromeZhu <JeromeZhu666@gmail.com>
2019-08-03 10:41:03 +0800

    完成设计模式-设计原则-开闭原则

---
+++ design-patterns/src/main/java/xin/jerome/sutdy/designpatterns/principle/openclose/StudentAnotherPursuance.java
@@ -0,0 +1,28 @@
+package xin.jerome.sutdy.designpatterns.principle.openclose;
+
+/**
+ * 学生同时有了两个追求
+ *
+ * @author Jerome Zhu
+ * @since 2019.08.03 09:42
+ */
+public class StudentAnotherPursuance extends StudentPursuance {
+
+    /** 另一个追求 */
+    private String anotherPursuance;
+
+    public StudentAnotherPursuance(Pursuance pursuance, String anotherPursuance) {
+        super(pursuance.getName(), pursuance.getAge(), pursuance.getCurrentPursuance());
+        this.anotherPursuance = anotherPursuance;
+    }
+
+    public String getAnotherPursuance() {
+        return anotherPursuance;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("姓名: %s ,年龄 : %d ,追求 : %s ,同时追求 : %s .", getName(), getAge(), getCurrentPursuance(),
+            anotherPursuance);
+    }
+}
