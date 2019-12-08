3ec758a16f8cd54a0c2eddcb4ef89e4cb8876306
Jerome <jeromezhu666@gmail.com>
2019-12-08 16:35:44 +0800

    完善策略模式描述

---
+++ design-patterns/src/main/java/xin/jerome/sutdy/designpatterns/principle/singleresponsibility/SingleResponsibilityTest.java
@@ -0,0 +1,12 @@
+package xin.jerome.sutdy.designpatterns.principle.singleresponsibility;
+
+/**
+ * 单一职责测试
+ *
+ * 单一职责原则 : 规定每个类都应该有一个单一的功能,并且该功能应该由这个类完全封装起来.
+ *
+ * @author Jerome Zhu
+ * @since 2019年12月08日 16:28
+ */
+public class SingleResponsibilityTest {
+}
