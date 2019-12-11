8fd5ada8ee4fc12c67c92b8979ba3f0868bc77d7
JeromeZhu <jeromezhu666@gmail.com>
2019-12-11 16:10:09 +0800

    进程创建的相关问题

--- interview/src/main/java/xin/jerome/study/interview/java/lang/ProcessCreationQuestions.java
+++ interview/src/main/java/xin/jerome/study/interview/java/lang/ProcessCreationQuestions.java
@@ -4,18 +4,22 @@ import java.io.IOException;
 import org.junit.Test;
 
 /**
- * 创建进程的方式
+ * 进程创建的相关问题
  *
  * @author Jerome Zhu
  * @since 2019年12月10日 18:02
  */
-public class CreateProcessMethods {
+public class ProcessCreationQuestions {
 
     @Test
     public void createProcess() {
         Runtime runtime = Runtime.getRuntime();
         try {
-            Process process = runtime.exec("calc");
+            // 打开计算器
+            String openCalculator = "calc";
+            // 打开网页
+            String openWebSite = "cmd /k start http://www.baidu.com";
+            Process process = runtime.exec(openWebSite);
             process.exitValue();
         } catch (IOException e) {
             e.printStackTrace();
