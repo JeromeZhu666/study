a466e750b127d3b970294956aeffc297f5a06d2f
Jerome <jeromezhu666@gmail.com>
2019-12-15 12:30:43 +0800

    线程通讯问题

---
+++ interview/src/main/java/xin/jerome/study/interview/java/lang/ThreadCommunicationQuestions.java
@@ -0,0 +1,11 @@
+package xin.jerome.study.interview.java.lang;
+
+/**
+ * 线程通讯问题
+ *   1.为什么wait()和notify()以及notifyAll()方法属于Object,并解释它们的作用.
+ *
+ * @author Jerome Zhu
+ * @since 2019年12月12日 21:28
+ */
+public class ThreadCommunicationQuestions {
+}
