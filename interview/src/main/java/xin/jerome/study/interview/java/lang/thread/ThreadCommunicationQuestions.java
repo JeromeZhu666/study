b219d1a0feaf4ae2611c0f81f1bbec7be192cdec
Jerome <jeromezhu666@gmail.com>
2019-12-16 22:16:55 +0800

    完善 线程通讯问题

--- interview/src/main/java/xin/jerome/study/interview/java/lang/thread/ThreadCommunicationQuestions.java
+++ interview/src/main/java/xin/jerome/study/interview/java/lang/thread/ThreadCommunicationQuestions.java
@@ -3,6 +3,9 @@ package xin.jerome.study.interview.java.lang.thread;
 /**
  * 线程通讯问题
  *   1.为什么wait()和notify()以及notifyAll()方法属于Object,并解释它们的作用.
+ *   2.为什么Object wait() 和notify() 以及notifyAll() 方法必须synchronized之中执行?
+ *      wait() 要去获取一把锁阻塞线程，notify() 释放锁随机唤醒一个阻塞线程，notifyAll() 释放锁唤醒所有阻塞线程.
+ *   3.请通过Java代码模拟实现wait()和notify() 以及notifyAll() 的语义?
  *
  * @author Jerome Zhu
  * @since 2019年12月12日 21:28
