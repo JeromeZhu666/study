4da8c0b4427ddeca75d1db11623bad1efcd2399b
JeromeZhu <jeromezhu666@gmail.com>
2019-12-18 18:03:39 +0800

    todo JUC 条件变量相关问题

---
+++ interview/src/main/java/xin/jerome/study/interview/java/util/concurrent/ConditionQuestions.java
@@ -0,0 +1,14 @@
+package xin.jerome.study.interview.java.util.concurrent;
+
+/**
+ * todo JUC 条件变量相关问题
+ *   1.请举例说明Condition使用场景?
+ *   2.请使用Condition实现"生产者-消费者问题"
+ *   3.请解释Condition await() 和signal() 与Object wait() 和notify() 的相同与差异?
+ *
+ * @author Jerome Zhu
+ * @since 2019年12月18日 17:05
+ */
+public class ConditionQuestions {
+
+}
