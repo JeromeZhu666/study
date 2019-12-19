ba15dab8968cb9b647436f891c9913679fd3b61a
JeromeZhu <jeromezhu666@gmail.com>
2019-12-19 11:28:25 +0800

    新增线程安全的Set实现方式

--- interview/src/main/java/xin/jerome/study/interview/java/util/ThreadSafeSetQuestions.java
+++ interview/src/main/java/xin/jerome/study/interview/java/util/ThreadSafeSetQuestions.java
@@ -15,6 +15,7 @@ import org.junit.Test;
  *     {@link ThreadSafeSetQuestions#testListAtLeastThreeThreadSafeSetImplementation()}
  *   2.在J.U.C框架中,存在HashSet的线程安全实现?如果不存在的话，要如何实现?
  *     不存在，可以利用 {@link ConcurrentHashMap} 模仿 {@link HashSet}的实现来实现.
+ *     或者用 Java 6 提供的 {@link Collections#newSetFromMap(java.util.Map)} 方法实现.
  *   todo 3.当Set#iterator() 方法返回Iterator对象后，能否在其迭代中,给Set对象添加新的元素?
  *     不一定，传统实现，会有fail-fast，而在 JUC 里是弱一直性，还是可以在迭代中添加元素的
  *
